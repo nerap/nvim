7b61fc45399a28f4b7eb63d3eba19d630293e23b
nerap <raphael@may.fr>
2024-06-11 23:42:12 +0200

    feat: perfect neovim atm

--- lua/main/lazy/dashboard.lua
+++ lua/main/lazy/dashboard.lua
@@ -50,21 +50,22 @@ return {
                         { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
                         { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
                         { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-                        {
-                            action = [[lua require("lazyvim.util").telescope.config_files()()]],
-                            desc = " Config",
-                            icon = " ",
-                            key = "c"
-                        },
-                        { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-                        { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
+                    --  {
+                    --      action = [[lua require("lazyvim.util").telescope.config_files()()]],
+                    --      desc = " Config",
+                    --      icon = " ",
+                    --      key = "c"
+                    --  },
+                    --  { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
+                    --  { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
                         { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
                         { action = "qa", desc = " Quit", icon = " ", key = "q" },
                     },
                     footer = function()
                         local stats = require("lazy").stats()
                         local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                        return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+                        return { "⚡ Neovim loaded in " .. ms .. "ms" }
+                        -- return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
                     end,
                 },
             }
