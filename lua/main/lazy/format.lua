a604f227fde150ab79c0971e11ee1a33eb92009a
nerap <raphael@may.fr>
2024-06-11 21:07:01 +0200

    feat: add fromat

---
+++ lua/main/lazy/format.lua
@@ -0,0 +1,86 @@
+return {
+    {
+        "mfussenegger/nvim-lint",
+        event = {
+            "BufReadPre",
+            "BufNewFile",
+        },
+        config = function()
+            local lint = require("lint")
+
+            lint.linters_by_ft = {
+                javascript = { "eslint_d" },
+                typescript = { "eslint_d" },
+                javascriptreact = { "eslint_d" },
+                typescriptreact = { "eslint_d" },
+                svelte = { "eslint_d" },
+                kotlin = { "ktlint" },
+                terraform = { "tflint" },
+                ruby = { "standardrb" },
+            }
+
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
+
+            vim.keymap.set("n", "<leader>ll", function()
+                lint.try_lint()
+            end, { desc = "Trigger linting for current file" })
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local conform = require("conform")
+
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    svelte = { { "prettierd", "prettier" } },
+                    javascript = { { "prettierd", "prettier" } },
+                    typescript = { { "prettierd", "prettier" } },
+                    javascriptreact = { { "prettierd", "prettier" } },
+                    typescriptreact = { { "prettierd", "prettier" } },
+                    json = { { "prettierd", "prettier" } },
+                    graphql = { { "prettierd", "prettier" } },
+                    java = { "google-java-format" },
+                    kotlin = { "ktlint" },
+                    ruby = { "standardrb" },
+                    markdown = { { "prettierd", "prettier" } },
+                    erb = { "htmlbeautifier" },
+                    html = { "htmlbeautifier" },
+                    bash = { "beautysh" },
+                    proto = { "buf" },
+                    rust = { "rustfmt" },
+                    yaml = { "yamlfix" },
+                    toml = { "taplo" },
+                    css = { { "prettierd", "prettier" } },
+                    scss = { { "prettierd", "prettier" } },
+                    sh = { { "shellcheck" } },
+                },
+            })
+
+            vim.keymap.set({ "n", "v" }, "<leader>l", function()
+                conform.format({
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 1000,
+                })
+            end, { desc = "Format file or range (in visual mode)" })
+
+            vim.keymap.set("n", "<leader>FF", function()
+                conform.format({
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 1000,
+                })
+            end, { desc = "Format file" })
+        end,
+    }
+}
