e2446751e8005a78c6d7b8e6bb9bc71dfb4bcb8d
nerap <44852526+nerap@users.noreply.github.com>
2024-06-22 23:21:13 +0200

    feat: show hidden default
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- lua/main/lazy/local.lua
+++ lua/main/lazy/local.lua
@@ -1,72 +1,73 @@
 local local_plugins = {
-    {
-        "harpoon",
-        dir = "~/personal/harpoon",
-        config = function()
-            local harpoon = require("harpoon")
+  {
+    "harpoon",
+    dir = "~/personal/harpoon",
+    config = function()
+      local harpoon = require("harpoon")
 
-            harpoon:setup()
+      harpoon:setup()
 
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
-        end
-    },
-    {
-        "vim-apm",
-        dir = "~/personal/vim-apm",
-        config = function()
-            --[[
-            local apm = require("vim-apm")
-
-            apm:setup({})
-            vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-            --]]
-        end
-    },
-    {
-        "windwp/nvim-ts-autotag",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-ts-autotag").setup({})
-        end,
-        lazy = true,
-        event = "VeryLazy",
-    },
-    {
-        "stevearc/oil.nvim",
-        opts = {},
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("oil").setup({
-                default_file_explorer = true,
-                delete_to_trash = true,
-                skip_confirm_for_simple_edits = true,
-                keymaps = {
-                    ["g?"] = "actions.show_help",
-                    ["<CR>"] = "actions.select",
-                    ["<C-s>"] = { "actions.select_split", opts = { vertical = true } },
-                    ["<C-h>"] = { "actions.select_split", opts = { horizontal = true } },
-                    ["<C-t>"] = { "actions.select_split", opts = { tab = true } },
-                    ["<C-p>"] = false,
-                    ["<C-c>"] = "actions.close",
-                    ["<C-l>"] = "actions.refresh",
-                    ["-"] = "actions.parent",
-                    ["_"] = "actions.open_cwd",
-                    ["`"] = "actions.cd",
-                    ["~"] = { "actions.cd", opts = { scope = "tab" } },
-                    ["gs"] = "actions.change_sort",
-                    ["gx"] = "actions.open_external",
-                    ["g."] = "actions.toggle_hidden",
-                    ["g\\"] = "actions.toggle_trash",
-                },
-            })
-        end,
-    },
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+    end
+  },
+--  {
+--    "vim-apm",
+--    dir = "~/personal/vim-apm",
+--    config = function()
+--      local apm = require("vim-apm")
+--
+--      apm:setup({})
+--      vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
+--    end
+--  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup({})
+    end,
+    lazy = true,
+    event = "VeryLazy",
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+        },
+        keymaps = {
+          ["g?"] = "actions.show_help",
+          ["<CR>"] = "actions.select",
+          ["<C-s>"] = false,
+          ["<C-h>"] = false,
+          ["<C-t>"] = false,
+          ["<C-p>"] = false,
+          ["<C-c>"] = "actions.close",
+          ["<C-l>"] = "actions.refresh",
+          ["-"] = "actions.parent",
+          ["_"] = "actions.open_cwd",
+          ["`"] = "actions.cd",
+          ["~"] = { "actions.cd", opts = { scope = "tab" } },
+          ["gs"] = "actions.change_sort",
+          ["gx"] = "actions.open_external",
+          ["g."] = "actions.toggle_hidden",
+          ["g\\"] = "actions.toggle_trash",
+        },
+      })
+    end,
+  },
 }
 
 return local_plugins
