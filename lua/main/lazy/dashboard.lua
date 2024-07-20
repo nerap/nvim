2514eae162f9a3700b62181871ba2935de933314
nerap <44852526+nerap@users.noreply.github.com>
2024-07-20 19:38:17 +0200

    feat: refacto folder
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- lua/main/lazy/dashboard.lua
+++ lua/main/lazy/dashboard.lua
@@ -1,28 +1,28 @@
 return {
-    {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        opts = {
-            routes = {
-                {
-                    filter = { event = "notify", find = "No information available" },
-                    opts = { skip = true },
-                },
-            },
-            presets = {
-                lsp_doc_border = true,
-            },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      routes = {
+        {
+          filter = { event = "notify", find = "No information available" },
+          opts = { skip = true },
         },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
-        },
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
     },
-    {
-        "nvimdev/dashboard-nvim",
-        event = "VimEnter",
-        opts = function()
-            local logo = [[
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function()
+      local logo = [[
                                                       
                ████ ██████           █████      ██
               ███████████             █████ 
@@ -33,102 +33,84 @@ return {
          ██████  █████████████████████ ████ █████ █████ ████ ██████
       ]]
 
-            logo = string.rep("\n", 8) .. logo .. "\n\n"
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-            local opts = {
-                theme = "doom",
-                hide = {
-                    -- this is taken care of by lualine
-                    -- enabling this messes up the actual laststatus setting after loading a file
-                    statusline = false,
-                },
-                config = {
-                    header = vim.split(logo, "\n"),
-                    -- stylua: ignore
-                    center = {
-                        { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
-                        { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
-                        { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
-                        { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
-                    --  {
-                    --      action = [[lua require("lazyvim.util").telescope.config_files()()]],
-                    --      desc = " Config",
-                    --      icon = " ",
-                    --      key = "c"
-                    --  },
-                    --  { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-                    --  { action = "LazyExtras", desc = " Lazy Extras", icon = " ", key = "x" },
-                        { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-                        { action = "qa", desc = " Quit", icon = " ", key = "q" },
-                    },
-                    footer = function()
-                        local stats = require("lazy").stats()
-                        local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-                        return { "⚡ Neovim loaded in " .. ms .. "ms" }
-                        -- return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-                    end,
-                },
-            }
+      local opts = {
+        theme = "doom",
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, "\n"),
+          -- stylua: ignore
+          center = {
+            { action = "Telescope find_files", desc = " Find file", icon = " ", key = "f" },
+            { action = "ene | startinsert", desc = " New file", icon = " ", key = "n" },
+            { action = "Telescope oldfiles", desc = " Recent files", icon = " ", key = "r" },
+            { action = "Telescope live_grep", desc = " Find text", icon = " ", key = "g" },
+            { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+            { action = "qa", desc = " Quit", icon = " ", key = "q" },
+          },
+          footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded in " .. ms .. "ms" }
+          end,
+        },
+      }
 
-            for _, button in ipairs(opts.config.center) do
-                button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-                button.key_format = "  %s"
-            end
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+        button.key_format = "  %s"
+      end
 
-            -- close Lazy and re-open when the dashboard is ready
-            if vim.o.filetype == "lazy" then
-                vim.cmd.close()
-                vim.api.nvim_create_autocmd("User", {
-                    pattern = "DashboardLoaded",
-                    callback = function()
-                        require("lazy").show()
-                    end,
-                })
-            end
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "DashboardLoaded",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
 
-            return opts
-        end,
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && yarn install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
-        "RRethy/vim-illuminate",
-        config = function()
-            require("illuminate")
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    theme = "tokyonight",
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = {"branch", "diff", "diagnostics" },
-                    lualine_c = { { "filename", path = 1 } },
-                    lualine_x = {
-                        { "fileformat", "filetype" },
-                        {
-                            require("noice").api.statusline.mode.get,
-                            cond = require("noice").api.statusline.mode.has,
-                            color = { fg = "#ff9e64" },
-                        },
-                    },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
-                },
-                extensions = { "fugitive", "quickfix", "fzf", "lazy", "mason", "nvim-dap-ui", "oil", "trouble" },
-            })
-        end,
-    },
+      return opts
+    end,
+  },
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require("illuminate")
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "tokyonight",
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { { "filename", path = 1 } },
+          lualine_x = {
+            { "fileformat", "filetype" },
+            {
+              require("noice").api.statusline.mode.get,
+              cond = require("noice").api.statusline.mode.has,
+              color = { fg = "#ff9e64" },
+            },
+          },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        extensions = { "fugitive", "quickfix", "fzf", "lazy", "mason", "nvim-dap-ui", "oil", "trouble" },
+      })
+    end,
+  },
 }
--- lua/main/lazy/format.lua
+++ lua/main/lazy/format.lua
@@ -1,86 +1,95 @@
 return {
-    {
-        "mfussenegger/nvim-lint",
-        event = {
-            "BufReadPre",
-            "BufNewFile",
-        },
-        config = function()
-            local lint = require("lint")
-
-            lint.linters_by_ft = {
-                javascript = { "eslint_d" },
-                typescript = { "eslint_d" },
-                javascriptreact = { "eslint_d" },
-                typescriptreact = { "eslint_d" },
-                svelte = { "eslint_d" },
-                kotlin = { "ktlint" },
-                terraform = { "tflint" },
-                ruby = { "standardrb" },
-            }
-
-            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-                group = lint_augroup,
-                callback = function()
-                    lint.try_lint(nil, { ignore_errors = true })
-                end,
-            })
-
-            vim.keymap.set("n", "<leader>ll", function()
-                lint.try_lint(nil, { ignore_errors = true })
-            end, { desc = "Trigger linting for current file" })
-        end,
+  {
+    "mfussenegger/nvim-lint",
+    event = {
+      "BufReadPre",
+      "BufNewFile",
     },
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local conform = require("conform")
+    config = function()
+      local lint = require("lint")
 
-            conform.setup({
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    svelte = { { "prettierd", "prettier" } },
-                    javascript = { { "prettierd", "prettier" } },
-                    typescript = { { "prettierd", "prettier" } },
-                    javascriptreact = { { "prettierd", "prettier" } },
-                    typescriptreact = { { "prettierd", "prettier" } },
-                    json = { { "prettierd", "prettier" } },
-                    graphql = { { "prettierd", "prettier" } },
-                    java = { "google-java-format" },
-                    kotlin = { "ktlint" },
-                    ruby = { "standardrb" },
-                    markdown = { { "prettierd", "prettier" } },
-                    erb = { "htmlbeautifier" },
-                    html = { "htmlbeautifier" },
-                    bash = { "beautysh" },
-                    proto = { "buf" },
-                    rust = { "rustfmt" },
-                    yaml = { "yamlfix" },
-                    toml = { "taplo" },
-                    css = { { "prettierd", "prettier" } },
-                    scss = { { "prettierd", "prettier" } },
-                    sh = { { "shellcheck" } },
-                },
-            })
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        svelte = { "eslint_d" },
+        kotlin = { "ktlint" },
+        terraform = { "tflint" },
+        ruby = { "standardrb" },
+      }
 
-            vim.keymap.set({ "n", "v" }, "<leader>l", function()
-                conform.format({
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 1000,
-                })
-            end, { desc = "Format file or range (in visual mode)" })
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-            vim.keymap.set("n", "<leader>f", function()
-                conform.format({
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 1000,
-                })
-            end, { desc = "Format file" })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint(nil, { ignore_errors = true })
         end,
-    }
+      })
+
+      vim.keymap.set("n", "<leader>ll", function()
+        lint.try_lint(nil, { ignore_errors = true })
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          svelte = { { "prettierd", "prettier" } },
+          javascript = { { "prettierd", "prettier" } },
+          typescript = { { "prettierd", "prettier" } },
+          javascriptreact = { { "prettierd", "prettier" } },
+          typescriptreact = { { "prettierd", "prettier" } },
+          json = { { "prettierd", "prettier" } },
+          graphql = { { "prettierd", "prettier" } },
+          java = { "google-java-format" },
+          kotlin = { "ktlint" },
+          ruby = { "standardrb" },
+          markdown = { { "prettierd", "prettier" } },
+          erb = { "htmlbeautifier" },
+          html = { "htmlbeautifier" },
+          bash = { "beautysh" },
+          proto = { "buf" },
+          rust = { "rustfmt" },
+          yaml = { "yamlfix" },
+          toml = { "taplo" },
+          css = { { "prettierd", "prettier" } },
+          scss = { { "prettierd", "prettier" } },
+          sh = { { "shellcheck" } },
+        },
+      })
+
+      vim.keymap.set({ "n", "v" }, "<leader>l", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file or range (in visual mode)" })
+
+      vim.keymap.set("n", "<leader>f", function()
+        conform.format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 1000,
+        })
+      end, { desc = "Format file" })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-ts-autotag").setup({})
+    end,
+    lazy = true,
+    event = "VeryLazy",
+  },
 }
