ded917adea78c845e10ae3d95f09ae87b7aca20c
nerap <44852526+nerap@users.noreply.github.com>
2024-08-24 00:26:45 +0200

    🔥 remove unsued codegen
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- lua/main/lazy/local.lua
+++ lua/main/lazy/local.lua
@@ -25,14 +25,4 @@ return {
       require("gitmoji").setup({})
     end
   }
-  --  {
-  --    "vim-apm",
-  --    dir = "~/personal/vim-apm",
-  --    config = function()
-  --      local apm = require("vim-apm")
-  --
-  --      apm:setup({})
-  --      vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-  --    end
-  --  },
 }
