5a32833ba6c620a7f311b065a4e414b282d33228
nerap <44852526+nerap@users.noreply.github.com>
2024-08-23 23:56:47 +0200

    ✨ add cloak telescope
    Signed-off-by: nerap <44852526+nerap@users.noreply.github.com>

--- lua/main/lazy/cloak.lua
+++ lua/main/lazy/cloak.lua
@@ -2,7 +2,7 @@ return {
     "laytan/cloak.nvim",
     config = function()
         require("cloak").setup({
-            enabled = false,
+            enabled = true,
             cloak_character = "*",
             -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
             highlight_group = "Comment",
