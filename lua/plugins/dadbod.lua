3c695e9b213f46b917268f2da2ff0c0675c5582e
nerap <44852526+nerap@users.noreply.github.com>
2024-09-15 16:33:56 +0200

    ✨ disable autofoldable

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -13,6 +13,16 @@ return {
     },
     init = function()
       -- Your DBUI configuration
+      vim.api.nvim_create_augroup('MyAutoCmdGroup', { clear = true })
+
+      -- Define the autocmd to set local options for 'dbout' filetype
+      vim.api.nvim_create_autocmd('FileType', {
+        group = 'MyAutoCmdGroup',
+        pattern = 'dbout',
+        callback = function()
+          vim.opt_local.foldenable = false
+        end
+      })
       vim.g.db_ui_use_nerd_fonts = 1
     end,
   }
