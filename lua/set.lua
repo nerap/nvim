890d754b5a721b0744ccc243259864aa63981b50
nerap <44852526+nerap@users.noreply.github.com>
2024-10-25 22:59:49 +0200

    autoindent

--- lua/set.lua
+++ lua/set.lua
@@ -4,11 +4,12 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 vim.opt.wrap = false
 
